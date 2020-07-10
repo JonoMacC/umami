15b248d45ff80c9e42075c3698487cafe6026b9d
JonoMacC <39803935+JonoMacC@users.noreply.github.com>
2020-07-10 11:52:32 -0700

    Add seamless local development

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,11 @@
-// const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
-// const yelp = "https://api.yelp.com/v3/businesses/";
+const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
+const yelp = "https://api.yelp.com/v3/businesses/";
+
+// Set the router base path
+// In development, use corsAnywhere to proxy requests
+// In production, redirects are handled by Netlify as specified in netlify.toml
+const routerBasePath =
+  process.env.NODE_ENV === "development" ? `${corsAnywhere}${yelp}` : `/yelp/`;
 
 const Yelp = {
   search(term, location, latitude, longitude, sortBy, limit) {
@@ -11,7 +17,7 @@ const Yelp = {
     }
 
     const query = `search?term=${term}${locationQuery}&sort_by=${sortBy}&limit=${limit}&categories=food,restaurants`;
-    return fetch(`/yelp/${query}`, {
+    return fetch(`${routerBasePath}${query}`, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
       },
